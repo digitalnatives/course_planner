37ca21bdb4a6ce3a3d38b2e377361df0114d6335
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-04-07 10:21:13 -0300

    Cleanup some comments.

--- config/config.exs
+++ config/config.exs
@@ -1,15 +1,8 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# General application configuration
 config :course_planner,
   ecto_repos: [CoursePlanner.Repo]
 
-# Configures the endpoint
 config :course_planner, CoursePlanner.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "Ml1LamAwEl2ra/9mM5mMbFffEqgL0vp05nbPjrXw0Mfzn0RO4x/t77NVPuZ5AzN+",
@@ -17,11 +10,8 @@ config :course_planner, CoursePlanner.Endpoint,
   pubsub: [name: CoursePlanner.PubSub,
            adapter: Phoenix.PubSub.PG2]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,11 +1,5 @@
 use Mix.Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with brunch.io to recompile .js and .css sources.
 config :course_planner, CoursePlanner.Endpoint,
   http: [port: 4000],
   debug_errors: true,
@@ -15,7 +9,6 @@ config :course_planner, CoursePlanner.Endpoint,
                     cd: Path.expand("../", __DIR__)]]
 
 
-# Watch static and templates for browser reloading.
 config :course_planner, CoursePlanner.Endpoint,
   live_reload: [
     patterns: [
@@ -26,14 +19,10 @@ config :course_planner, CoursePlanner.Endpoint,
     ]
   ]
 
-# Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
-# Set a higher stacktrace during development. Avoid configuring such
-# in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
 config :course_planner, CoursePlanner.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
