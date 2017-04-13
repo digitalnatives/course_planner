049a525dfcce8b7a8531e8a282b78ed09d78e4e5
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-04-13 13:42:51 -0300

    Create actions show, edit/update and delete.

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -34,4 +34,28 @@ defmodule CoursePlanner.UserControllerTest do
     assert html_response(conn, 200) =~ "Show user"
   end
 
+  test "renders page not found when id is nonexistent", %{conn: conn} do
+    assert_error_sent 404, fn ->
+      get conn, user_path(conn, :show, -1)
+    end
+  end
+
+  test "renders form for editing chosen resource", %{conn: conn} do
+    user = Repo.insert! %User{}
+    conn = get conn, user_path(conn, :edit, user)
+    assert html_response(conn, 200) =~ "Edit user"
+  end
+
+  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
+    user = Repo.insert! %User{}
+    conn = put conn, user_path(conn, :update, user), user: @invalid_attrs
+    assert html_response(conn, 200) =~ "Edit user"
+  end
+
+  test "deletes chosen resource", %{conn: conn} do
+    user = Repo.insert! %User{}
+    conn = delete conn, user_path(conn, :delete, user)
+    assert redirected_to(conn) == user_path(conn, :index)
+    refute Repo.get(User, user.id)
+  end
 end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -20,4 +20,33 @@ defmodule CoursePlanner.UserController do
     render(conn, "show.html", user: user)
   end
 
+  def edit(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user)
+    render(conn, "edit.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user, user_params)
+
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User updated successfully.")
+        |> redirect(to: user_path(conn, :show, user))
+      {:error, changeset} ->
+        render(conn, "edit.html", user: user, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+
+    Repo.delete!(user)
+
+    conn
+    |> put_flash(:info, "User deleted successfully.")
+    |> redirect(to: user_path(conn, :index))
+  end
 end
