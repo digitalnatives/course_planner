fc5015e0d1d754ef9118b4d3b144eeccf55e5be1
Guilherme <ggpasqualino@gmail.com>
2017-04-13 10:04:25 +0200

    Add reference for coherence dependency

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule CoursePlanner.Mixfile do
      {:cowboy, "~> 1.0"},
      {:credo, "~> 0.7.2", only: [:dev, :test]},
      {:dogma, "~> 0.1.14", only: [:dev, :test]},
-     {:coherence, github: "smpallen99/coherence"},
+     {:coherence, github: "smpallen99/coherence", ref: "f67b3c0"},
    ]
   end
 
