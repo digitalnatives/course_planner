59c8ae2d69d3ab809ea7610fc1b62ae5a8dc817f
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-04-21 11:21:58 +0200

    Fixes the comments given on the previous PR

--- web/models/types/entity_status.ex
+++ web/models/types/entity_status.ex
@@ -1,26 +1,26 @@
-defmodule CoursePlanner.Types.EntityType do
+defmodule CoursePlanner.Types.EntityStatus do
   @moduledoc """
     This module introduces a custom type for Etco for checking status in the model
   """
   @behaviour Ecto.Type
-  def type, do: :string
+  def type, do: :entity_status
 
-  @valid_entity_types ['Planned', 'Active', 'Finished', 'Graduated', 'Frozen', 'Deleted']
+  @valid_entity_types ["Planned", "Active", "Finished", "Graduated", "Frozen", "Deleted"]
 
-  def valid_entity_types, do: @valid_entity_types
+  def values, do: @valid_entity_types
 
   def cast(value) do
     case Enum.member?(@valid_entity_types, value) do
-      :true  -> value
+      :true  -> {:ok, value}
       :false -> :error
     end
   end
 
-  def load(value), do: value
+  def load(value), do: {:ok, value}
 
   def dump(value) do
     case Enum.member?(@valid_entity_types, value) do
-      :true  -> value
+      :true  -> {:ok, value}
       :false -> :error
     end
   end
