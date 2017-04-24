4a53208ddaf91a38a76d761a993a79acd24771d5
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-04-24 07:32:26 -0300

    List only not deleted users.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -7,7 +7,8 @@ defmodule CoursePlanner.UserController do
   require Logger
 
   def index(conn, _params) do
-    users = Repo.all(User)
+    query = from u in User, where: u.deleted == false
+    users = Repo.all(query)
     render(conn, "index.html", users: users)
   end
 
@@ -66,11 +67,20 @@ defmodule CoursePlanner.UserController do
 
   def delete(conn, %{"id" => id}) do
     user = Repo.get!(User, id)
+    changeset = User.changeset(user,
+      %{deleted: true, deleted_at: Ecto.DateTime.utc()},
+      :delete)
 
-    Repo.delete!(user)
-
-    conn
-    |> put_flash(:info, "User deleted successfully.")
-    |> redirect(to: user_path(conn, :index))
+    IO.inspect changeset
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User deleted successfully.")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Something went wrong.")
+        |> redirect(to: user_path(conn, :index))
+    end
   end
 end
