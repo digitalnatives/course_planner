d09494c57b57b6e69f50446aba6e0c0127293d8e
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-05-04 10:53:08 -0300

    Use macro to generalize behaviour.

--- lib/enum_timestamp.ex
+++ lib/enum_timestamp.ex
@@ -2,6 +2,25 @@ defmodule EnumTimestamp do
   @moduledoc """
   Behaviour to represent Enum with mapping to timestamp
   """
-  @callback valid_types() :: [String.t]
-  @callback types_timestamp() :: [{String.t, atom()}]
+  defmacro __using__(_) do
+    quote do
+      @behaviour Ecto.Type
+      @behaviour EnumTimestamp
+      @callback valid_types :: [String.t]
+      def cast(value) do
+        if Enum.member?(valid_types(), value), do: {:ok, value}, else: :error
+      end
+
+      def load(value), do: {:ok, value}
+
+      def dump(value) do
+        if Enum.member?(valid_types(), value), do: {:ok, value}, else: :error
+      end
+
+      def types_timestamp do
+        Enum.into(valid_types(), %{}, &({&1, :"#{String.downcase(&1)}_at"}))
+      end
+      defoverridable types_timestamp: 0
+    end
+  end
 end
--- web/models/types/entity_status.ex
+++ web/models/types/entity_status.ex
@@ -2,27 +2,9 @@ defmodule CoursePlanner.Types.EntityStatus do
   @moduledoc """
     This module introduces a custom type for Etco for checking status in the model
   """
-  @behaviour Ecto.Type
-  @behaviour EnumTimestamp
+  use EnumTimestamp
+
   def type, do: :entity_status
-
-  @valid_entity_types ["Planned", "Active", "Finished", "Graduated", "Frozen"]
-
-  def valid_entity_types, do: @valid_entity_types
-
-  def values, do: @valid_entity_types
-
-  def cast(value) when value in @valid_entity_types, do: {:ok, value}
-  def cast(_value), do: :error
-
-  def load(value), do: {:ok, value}
-
-  def dump(value) when value in @valid_entity_types, do: {:ok, value}
-  def dump(_value), do: :error
-
-  def valid_types, do: @valid_entity_types
-  def types_timestamp do
-    Enum.into(valid_types(), %{}, &({&1, :"#{String.downcase(&1)}_at"}))
-  end
+  def valid_types, do: ["Planned", "Active", "Finished", "Graduated", "Frozen"]
 
 end
