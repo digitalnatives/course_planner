57e7b07f3423e1c03e19355681103e1a2a4ae723
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-05-03 21:38:46 -0300

    Fix credo warnings.

--- lib/enum_timestamp.ex
+++ lib/enum_timestamp.ex
@@ -1,4 +1,7 @@
 defmodule EnumTimestamp do
+  @moduledoc """
+  Behaviour to represent Enum with mapping to timestamp
+  """
   @callback valid_types() :: [String.t]
   @callback types_timestamp() :: [{String.t, atom()}]
 end
