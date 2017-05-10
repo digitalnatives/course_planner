7770c414b7e4998987aa81b7d4ebc587ea4ced96
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-05-10 14:35:14 -0300

    Extract assigned user to view.

--- web/views/task_view.ex
+++ web/views/task_view.ex
@@ -1,4 +1,12 @@
 defmodule CoursePlanner.TaskView do
   use CoursePlanner.Web, :view
 
+  def task_user(conn, task) do
+    if task.user_id do
+      link task.user.name, to: volunteer_path(conn, :show, task.user)
+    else
+      "no one"
+    end
+  end
+
 end
