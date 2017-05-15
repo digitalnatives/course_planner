7b740230d3a40caf9ee6a1809a5cd2fa1d813fa7
Guilherme <ggpasqualino@gmail.com>
2017-05-15 13:39:09 +0200

    Add user to the index page test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,6 +1,23 @@
 defmodule CoursePlanner.PageControllerTest do
   use CoursePlanner.ConnCase
 
+  alias CoursePlanner.User
+
+  setup do
+    user =
+      %User{
+        name: "Test User",
+        email: "testuser@example.com",
+        password: "secret",
+        password_confirmation: "secret"
+      }
+
+    conn =
+      Phoenix.ConnTest.build_conn()
+      |> assign(:current_user, user)
+    {:ok, conn: conn}
+  end
+
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
