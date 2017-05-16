c09aef1964bd871b51b0cb4b6324d64fbd12154e
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-05-16 10:38:04 +0200

    Move non-controller realed test to teachers_test.exs

--- test/controllers/teacher_controller_test.exs
+++ test/controllers/teacher_controller_test.exs
@@ -1,8 +1,7 @@
 defmodule CoursePlanner.TeacherControllerTest do
   use CoursePlanner.ConnCase
 
-  alias CoursePlanner.{Course, OfferedCourse, Repo, User, Teachers}
-  alias CoursePlanner.Terms.Term
+  alias CoursePlanner.{Repo, User, Teachers}
 
   @valid_attrs %{name: "some content", email: "valid@email"}
   @invalid_attrs %{}
@@ -11,41 +10,6 @@ defmodule CoursePlanner.TeacherControllerTest do
     email: "testuser@example.com",
     password: "secret",
     password_confirmation: "secret"}
-
-  defp create_term(name, start_date, end_date, course) do
-    Repo.insert!(
-      %Term{
-        name: name,
-        start_date: start_date,
-        end_date: end_date,
-        courses: [course],
-        status: "Planned"
-      })
-  end
-
-  defp create_course(name) do
-    Repo.insert!(
-      Course.changeset(
-        %Course{},
-        %{
-          name: name,
-          description: "Description",
-          number_of_sessions: 1,
-          session_duration: "01:00:00",
-          status: "Active"
-        }))
-  end
-
-  defp create_offered_course(term, course, teachers) do
-    Repo.insert!(
-      %OfferedCourse
-      {
-        term_id: term.id,
-        course_id: course.id,
-        teachers: teachers
-      }
-    )
-  end
 
   setup do
     conn =
@@ -100,45 +64,5 @@ defmodule CoursePlanner.TeacherControllerTest do
   test "renders form for new resources", %{conn: conn} do
     conn = get conn, teacher_path(conn, :new)
     assert html_response(conn, 200) =~ "New teacher"
-  end
-
-  test "gets empty list when teacher has no course assigned" do
-    {:ok, teacher} = Teachers.new(@valid_attrs, "whatever")
-    assert Teachers.courses(teacher.id) == []
-  end
-
-  test "gets list of courses when teacher has assigned courses" do
-    course = create_course("english")
-    term = create_term("FALL",
-                       %Ecto.Date{day: 1, month: 1, year: 2017},
-                       %Ecto.Date{day: 1, month: 6, year: 2017},
-                       course)
-    {:ok, teacher} = Teachers.new(@valid_attrs, "whatever")
-    create_offered_course(term, course, [teacher])
-    teacher_courses = Teachers.courses(teacher.id)
-
-    assert course.id == List.first(teacher_courses).course.id
-    assert term.id == List.first(teacher_courses).term.id
-  end
-
-  test "gets list of courses ordered descendingly by term starting_date when teacher has multiple courses assigned to" do
-    course = create_course("english")
-    term1 = create_term("FALL",
-                       %Ecto.Date{day: 1, month: 1, year: 2017},
-                       %Ecto.Date{day: 1, month: 6, year: 2017},
-                       course)
-    {:ok, teacher} = Teachers.new(@valid_attrs, "whatever")
-    term2 = create_term("FALL",
-                       %Ecto.Date{day: 1, month: 1, year: 2018},
-                       %Ecto.Date{day: 1, month: 6, year: 2018},
-                       course)
-    create_offered_course(term1, course, [teacher])
-    create_offered_course(term2, course, [teacher])
-    teacher_courses = Teachers.courses(teacher.id)
-
-    assert course.id == List.first(teacher_courses).course.id
-    assert term2.id == List.first(teacher_courses).term.id
-    assert course.id == List.last(teacher_courses).course.id
-    assert term1.id == List.last(teacher_courses).term.id
   end
 end
