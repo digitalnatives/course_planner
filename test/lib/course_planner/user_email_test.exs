efc39e19034edddd3219dbaca6360d2f58c8af34
Rodrigo Nonose <rodrigo.nonose@digitalnatives.hu>
2017-05-17 13:37:57 -0300

    Build email for user profile change.

---
+++ test/lib/course_planner/user_email_test.exs
@@ -0,0 +1,26 @@
+defmodule CoursePlanner.UserEmailTest do
+  use ExUnit.Case
+  doctest CoursePlanner.Mailer.UserEmail
+
+  alias CoursePlanner.{User, Mailer, Mailer.UserEmail}
+  import Swoosh.TestAssertions
+
+  @valid_user %User{name: "mahname", email: "valid@email"}
+  @invalid_user %User{name: "mahname"}
+
+  test "inexistent notification type" do
+    assert UserEmail.build_email(@valid_user, :sometype) == {:error, :wrong_notification_type}
+  end
+
+  test "invalid user" do
+    assert UserEmail.build_email(@invalid_user, :user_modified) == {:error, :invalid_recipient}
+  end
+
+  test "notify user update" do
+    email = @valid_user
+    |> UserEmail.build_email(:user_modified)
+    |> Mailer.deliver()
+    assert_email_sent subject: "Your profile was updated"
+  end
+
+end
