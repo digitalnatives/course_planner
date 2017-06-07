711929d747bf9d009e9f97683678cd1540a7b8cf
László Kardinál <laszlo.kardinal@digitalnatives.hu>
2017-06-07 15:01:47 +0200

    Added error logger to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -49,6 +49,13 @@ gulp.task('css-app', function() {
     .pipe( concat('app.scss') )
     .pipe( sass( { importer: moduleImporter() } ).on('error', sass.logError ) )
     .pipe( gutil.env.env === 'production' ? cleanCSS() : gutil.noop() )
+    .on( "error",
+      function ( err ) {
+        gutil.log(
+          gutil.colors.red( "[Error]" ), JSON.stringify( err, 0, 2 )
+        );
+      }
+    )
     .pipe( gulp.dest('priv/static/css') )
 });
 
@@ -59,12 +66,26 @@ gulp.task('js-app', function() {
     .pipe( babel({presets: ['es2015']}) )
     .pipe( browserify( { debug: gutil.env.env !== 'production' } ) )
     .pipe( gutil.env.env === 'production' ? uglify() : gutil.noop() )
+    .on( "error",
+      function ( err ) {
+        gutil.log(
+          gutil.colors.red( "[Error]" ), JSON.stringify( err, 0, 2 )
+        );
+      }
+    )
     .pipe( gulp.dest('priv/static/js') )
 });
 
 gulp.task('assets', function() {
   return gulp
     .src( assetPaths )
+    .on( "error",
+      function ( err ) {
+        gutil.log(
+          gutil.colors.red( "[Error]" ), JSON.stringify( err, 0, 2 )
+        );
+      }
+    )
     .pipe( gulp.dest('priv/static') );
 });
 
