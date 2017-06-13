9df67145542388759feed25304d7be96bcb14108
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-06-13 10:39:43 +0200

    removing holiday test from term test file

--- test/models/term_test.exs
+++ test/models/term_test.exs
@@ -20,35 +20,6 @@ defmodule CoursePlanner.TermTest do
     refute changeset.valid?
   end
 
-  test "changeset with valid holidays" do
-    valid_attrs =
-      %{
-        name: "Spring",
-        start_date: %{day: 17, month: 4, year: 2010},
-        end_date: %{day: 17, month: 5, year: 2017},
-        holidays:
-          [
-            %{name: "Labor Day", date: %{day: 01, month: 5, year: 2015}}
-          ]
-      }
-    changeset = Term.changeset(%Term{}, valid_attrs)
-    assert changeset.valid?
-  end
-
-  test "changeset with invalid holidays" do
-    invalid_attrs =
-      %{
-        start_date: %{day: 17, month: 4, year: 2010},
-        end_date: %{day: 17, month: 4, year: 2010},
-        holidays:
-          [
-            %{name: "Labor Day", date: nil}
-          ]
-      }
-    changeset = Term.changeset(%Term{}, invalid_attrs)
-    refute changeset.valid?
-  end
-
   test "start_date same as end_date" do
     invalid_attrs =
       %{
