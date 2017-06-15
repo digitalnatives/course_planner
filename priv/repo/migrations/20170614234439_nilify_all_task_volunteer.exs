9c03068fe35a20ef959a1f5475be04b62b21d7ed
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-06-14 21:31:33 -0300

    Fix weird behavior on migration.

--- priv/repo/migrations/20170614234439_nilify_all_task_volunteer.exs
+++ priv/repo/migrations/20170614234439_nilify_all_task_volunteer.exs
@@ -4,7 +4,8 @@ defmodule CoursePlanner.Repo.Migrations.NilifyAllTaskVolunteer do
   def change do
     drop_if_exists index(:tasks, [:user_id])
     alter table(:tasks) do
-      modify :user_id, references(:users, on_delete: :nilify_all)
+      remove :user_id
+      add :user_id, references(:users, on_delete: :nilify_all)
     end
   end
 end
