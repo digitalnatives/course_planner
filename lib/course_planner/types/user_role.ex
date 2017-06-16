0c5c0a2a4c01dc9dda4fe6c9a61cd110c3536939
Guilherme <ggpasqualino@gmail.com>
2017-06-16 10:54:45 +0200

    Refactor ParticipationType and UserRole to use CoursePlanner.Enum

--- lib/course_planner/types/user_role.ex
+++ lib/course_planner/types/user_role.ex
@@ -2,26 +2,8 @@ defmodule CoursePlanner.Types.UserRole do
   @moduledoc """
     This module introduces a custom type for Etco for checking user role in the user model
   """
-  @behaviour Ecto.Type
+  use CoursePlanner.Enum
+
   def type, do: :user_role
-
-  @valid_values ["Student", "Teacher", "Coordinator", "Volunteer"]
-
-  def values, do: @valid_values
-
-  def cast(value) do
-    case Enum.member?(@valid_values, value) do
-      :true  -> {:ok, value}
-      :false -> :error
-    end
-  end
-
-  def load(value), do: {:ok, value}
-
-  def dump(value) do
-    case Enum.member?(@valid_values, value) do
-      :true  -> {:ok, value}
-      :false -> :error
-    end
-  end
+  def valid_types, do: ["Student", "Teacher", "Coordinator", "Volunteer"]
 end
