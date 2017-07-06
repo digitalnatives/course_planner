da8d4d525b008dedb164bec5b442dd43ed3557f0
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-07-06 11:39:10 +0200

    uses Changeset.get_field instead of Map.get

--- web/controllers/calendar_controller.ex
+++ web/controllers/calendar_controller.ex
@@ -2,14 +2,15 @@ defmodule CoursePlanner.CalendarController do
   use CoursePlanner.Web, :controller
 
   alias CoursePlanner.CalenderHelper
+  alias Ecto.Changeset
 
   def index(%{assigns: %{current_user: current_user}} = conn, params) do
     changeset = CalenderHelper.validate(params)
 
     case changeset.valid? do
      true ->
-       my_classes = Map.get(changeset.changes, :my_classes) || false
-       date = Map.get(changeset.changes, :date) || Date.utc_today()
+       my_classes = Changeset.get_field(changeset, :my_classes, false)
+       date = Changeset.get_field(changeset, :date, Date.utc_today())
 
        week_range =  CalenderHelper.get_week_range(date)
        offered_courses = CalenderHelper.get_user_classes(current_user, my_classes, week_range)
