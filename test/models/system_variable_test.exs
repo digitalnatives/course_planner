ce36a88e1e0fa0bce982cb57cb4d21d82fefd06f
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-07-10 18:43:54 +0200

    Adds test to cover get_value function

--- test/models/system_variable_test.exs
+++ test/models/system_variable_test.exs
@@ -1,7 +1,9 @@
 defmodule CoursePlanner.SystemVariableTest do
   use CoursePlanner.ModelCase
 
-  alias CoursePlanner.SystemVariable
+  alias CoursePlanner.{SystemVariable, Settings}
+
+  import CoursePlanner.Factory
 
   @string_valid_attrs %{key: "sample key", value: "sample value", type: "string", visible: true, editable: true}
   @integer_valid_attrs %{key: "sample key", value: "1", type: "integer", visible: true, editable: true}
@@ -188,6 +190,32 @@ defmodule CoursePlanner.SystemVariableTest do
     test "changeset fails when input is empty" do
       changeset = SystemVariable.changeset(%SystemVariable{}, %{@string_valid_attrs | value: ""})
       refute changeset.valid?
+    end
+  end
+
+  describe "test the get_value" do
+    test "when variable type is string" do
+      value = "random value"
+      insert(:system_variable, %{key: "TEST_KEY", value: value, type: "string"})
+      assert value == Settings.get_value("TEST_KEY")
+    end
+
+    test "when variable type is integer" do
+      value = 10
+      insert(:system_variable, %{key: "TEST_KEY", value: "#{value}", type: "integer"})
+      assert value == Settings.get_value("TEST_KEY")
+    end
+
+    test "when variable type is boolean" do
+      value = true
+      insert(:system_variable, %{key: "TEST_KEY", value: "#{value}", type: "boolean"})
+      assert value == Settings.get_value("TEST_KEY")
+    end
+
+    test "when variable type is csv" do
+      value = "value1, value2"
+      insert(:system_variable, %{key: "TEST_KEY", value: value, type: "csv"})
+      assert ["value1", "value2"] == Settings.get_value("TEST_KEY")
     end
   end
 end
