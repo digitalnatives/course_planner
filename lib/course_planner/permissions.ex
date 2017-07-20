5b2dfaf8821acb589e6fbeb0bfef9260c19c755b
Guilherme Pasqualino <guilherme.pasqualino@digitalnatives.hu>
2017-07-20 11:16:42 +0200

    Blacklist permission by default

--- lib/course_planner/permissions.ex
+++ lib/course_planner/permissions.ex
@@ -1,58 +1,34 @@
 defimpl Canada.Can, for: CoursePlanner.User do
   alias CoursePlanner.{
-    Tasks.Task, Terms.Term, User, Course, Class,
-    AttendanceController, OfferedCourseController, SettingController
+    AttendanceController,
+    OfferedCourseController,
+    ScheduleController,
+    Tasks.Task,
+    User,
   }
 
-  def can?(%User{role: "Coordinator"}, _, _), do: true
+  def can?(%User{role: "Coordinator"}, _action, _controller), do: true
 
   def can?(%User{role: role}, action, AttendanceController)
     when role in ["Teacher", "Coordinator"] and
          action in [:show, :index, :fill_course, :update_fill], do: true
   def can?(%User{role: "Student"}, action, AttendanceController)
     when action in [:show, :index], do: true
-  def can?(_role, _action, AttendanceController), do: false
 
   def can?(%User{role: "Teacher"}, action, OfferedCourseController)
     when action in [:index, :show, :edit, :update], do: true
   def can?(%User{role: "Student"}, action, OfferedCourseController)
     when action in [:index, :show], do: true
-  def can?(_role, _action, OfferedCourseController), do: false
 
   def can?(_user, _action, ScheduleController), do: true
 
-  def can?(%User{role: role}, _action, Term)
-    when role in ["Teacher", "Student", "Volunteer"], do: false
-  def can?(%User{role: role}, _action, %Term{})
-    when role in ["Teacher", "Student", "Volunteer"], do: false
-
-  def can?(%User{role: role}, _action, Class)
-    when role in ["Teacher", "Student", "Volunteer"], do: false
-  def can?(%User{role: role}, _action, %Class{})
-   when role in ["Teacher", "Student", "Volunteer"], do: false
-
-  def can?(%User{role: role}, _action, Course)
-    when role in ["Teacher", "Student", "Volunteer"], do: false
-  def can?(%User{role: role}, _action, %Course{})
-    when role in ["Teacher", "Student", "Volunteer"], do: false
-
   def can?(%User{id: id}, action, %User{id: id})
     when action in [:show, :edit, :update], do: true
-  def can?(%User{role: role}, _action, User)
-    when role in ["Teacher", "Student", "Volunteer"], do: false
-  def can?(%User{role: role}, _action, %User{})
-    when role in ["Teacher", "Student", "Volunteer"], do: false
-
-  def can?(_role, _action, SettingController), do: false
 
   def can?(%User{role: "Volunteer", id: id}, :show, %Task{user_id: id}), do: true
   def can?(%User{role: "Volunteer"}, :index, Task), do: true
   def can?(%User{role: "Volunteer"}, :grab, %Task{}), do: true
   def can?(%User{role: "Volunteer"}, :show, %Task{user_id: nil}), do: true
-  def can?(%User{role: role}, _, Task)
-    when role in ["Teacher", "Student", "Volunteer"], do: false
-  def can?(%User{role: role}, _, %Task{})
-    when role in ["Teacher", "Student", "Volunteer"], do: false
 
-  def can?(_, _, _), do: true
+  def can?(_user, _action, _controller), do: false
 end
