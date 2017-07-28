3914cf1ccca66bc13534f1c8068b33b17f8c5aa1
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-07-28 14:36:13 -0300

    Refactor user email tests.

--- test/lib/course_planner/user_email_test.exs
+++ test/lib/course_planner/user_email_test.exs
@@ -21,75 +21,25 @@ defmodule CoursePlanner.UserEmailTest do
     assert UserEmail.build_email(%Notification{to: @invalid_user2, type: :user_modified}) == {:error, :invalid_recipient}
   end
 
-  test "notify user update" do
-    Notification.new()
-    |> Notification.to(@valid_user)
-    |> Notification.type(:user_modified)
-    |> UserEmail.build_email()
-    |> Mailer.deliver()
-    assert_email_sent subject: "Your profile is updated"
-  end
-
-  test "notify course update" do
-    Notification.new()
-    |> Notification.to(@valid_user)
-    |> Notification.type(:course_updated)
-    |> UserEmail.build_email()
-    |> Mailer.deliver()
-    assert_email_sent subject: "A course you subscribed to was updated"
-  end
-
-  test "notify course deleted" do
-    Notification.new()
-    |> Notification.to(@valid_user)
-    |> Notification.type(:course_deleted)
-    |> UserEmail.build_email()
-    |> Mailer.deliver()
-    assert_email_sent subject: "A course you subscribed to was deleted"
-  end
-
-  test "notify term updated" do
-    Notification.new()
-    |> Notification.to(@valid_user)
-    |> Notification.type(:term_updated)
-    |> UserEmail.build_email()
-    |> Mailer.deliver()
-    assert_email_sent subject: "A term you are enrolled in was updated"
-  end
-
-  test "notify term deleted" do
-    Notification.new()
-    |> Notification.to(@valid_user)
-    |> Notification.type(:term_deleted)
-    |> UserEmail.build_email()
-    |> Mailer.deliver()
-    assert_email_sent subject: "A term you are enrolled in was deleted"
-  end
-
-  test "notify class subscription" do
-    Notification.new()
-    |> Notification.to(@valid_user)
-    |> Notification.type(:class_subscribed)
-    |> UserEmail.build_email()
-    |> Mailer.deliver()
-    assert_email_sent subject: "You were subscribed to a class"
-  end
-
-  test "notify class updated" do
-    Notification.new()
-    |> Notification.to(@valid_user)
-    |> Notification.type(:class_updated)
-    |> UserEmail.build_email()
-    |> Mailer.deliver()
-    assert_email_sent subject: "A class you subscribe to was updated"
-  end
-
-  test "notify class deleted" do
-    Notification.new()
-    |> Notification.to(@valid_user)
-    |> Notification.type(:class_deleted)
-    |> UserEmail.build_email()
-    |> Mailer.deliver()
-    assert_email_sent subject: "A class you subscribe to was deleted"
+  for email <- [
+    {:user_modified, "Your profile is updated"},
+    {:course_updated, "A course you subscribed to was updated"},
+    {:course_deleted, "A course you subscribed to was deleted"},
+    {:term_updated, "A term you are enrolled in was updated"},
+    {:term_deleted, "A term you are enrolled in was deleted"},
+    {:class_subscribed, "You were subscribed to a class"},
+    {:class_updated, "A class you subscribe to was updated"},
+    {:class_deleted, "A class you subscribe to was deleted"}
+    ] do
+    @email email
+    test "notify #{inspect @email}" do
+      {type, subject} = @email
+      Notification.new()
+      |> Notification.to(@valid_user)
+      |> Notification.type(type)
+      |> UserEmail.build_email()
+      |> Mailer.deliver()
+      assert_email_sent subject: subject
+    end
   end
 end
