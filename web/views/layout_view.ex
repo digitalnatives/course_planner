4f95404f2bc1bd30f33059f644746b00014446c2
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-07-31 16:48:27 +0200

    Fixing the show page

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -7,4 +7,8 @@ defmodule CoursePlanner.LayoutView do
   def show_program_about? do
     Settings.get_value("SHOW_PROGRAM_ABOUT_PAGE")
   end
+
+  def get_program_name do
+    Settings.get_value("PROGRAM_NAME")
+  end
 end
