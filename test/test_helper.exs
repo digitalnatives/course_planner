0674a017cbbc2e64a62bb9871f4606a4f9b10996
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-08-03 09:29:59 -0300

    Remove unnecessary require.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,6 +3,3 @@
 ExUnit.start
 
 Ecto.Adapters.SQL.Sandbox.mode(CoursePlanner.Repo, :manual)
-
-Code.compiler_options(ignore_module_conflict: true)
-Code.require_file("test/support/test_notifier.ex")
