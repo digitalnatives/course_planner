914516b0692a8002d39b545b5f3e73653d8a9351
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-08-07 10:55:15 +0200

    Incorporates comments
    1 - change  "get_form_component_from_type " name to "setting_input"
    2 - users "SharedView.form_text" for the default option
    3 - removes the extra file and function

--- web/views/setting_view.ex
+++ web/views/setting_view.ex
@@ -3,7 +3,7 @@ defmodule CoursePlanner.SettingView do
   use CoursePlanner.Web, :view
   alias CoursePlanner.SharedView
 
-  def get_form_component_from_type(form, field, type, label) do
+  def setting_input(form, field, type, label) do
     case type do
       "text"    -> SharedView.form_textarea(form, field, label: label)
       "string"  -> SharedView.form_text(form, field, label: label)
@@ -11,11 +11,7 @@ defmodule CoursePlanner.SettingView do
       "list"    -> SharedView.form_text(form, field, label: label)
       "integer" -> SharedView.form_text(form, field, label: label)
       "boolean" -> SharedView.form_select(form, field, ["True", "False"], label: label)
-      _         -> render_default_setting_input(form)
+      _         -> SharedView.form_text(form, field, label: label)
     end
-  end
-
-  defp render_default_setting_input(form) do
-    render "default_setting_input.html", form: form
   end
 end
