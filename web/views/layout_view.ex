e5a34abe1290cb937a3a6de5fb80b218a715392d
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-08-11 10:44:11 +0200

    show different navbar based on user role access

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -11,4 +11,13 @@ defmodule CoursePlanner.LayoutView do
   def get_program_name do
     Settings.get_value("PROGRAM_NAME")
   end
+
+  def render_user_role_based_navbar(%{assigns: %{current_user: current_user}} = conn) do
+    case current_user.role do
+      "Coordinator" -> render("coordinator_app_navbar.html", conn: conn)
+      "Student" -> render("student_app_navbar.html", conn: conn)
+      "Teacher" -> render("teacher_app_navbar.html", conn: conn)
+      "Volunteer" -> render("volunteer_app_navbar.html", conn: conn)
+    end
+  end
 end
