685d01c00b384f0a27a23f8fa6f45be5ac7409bd
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-08-30 10:23:33 -0300

    Group user migration files.

--- priv/repo/migrations/20170503181307_rename_user_status.exs
+++
@@ -1,8 +0,0 @@
-defmodule CoursePlanner.Repo.Migrations.RenameUserStatus do
-  use Ecto.Migration
-
-  def change do
-    rename table(:users), :activated_at, to: :active_at
-    rename table(:users), :froze_at, to: :frozen_at
-  end
-end
