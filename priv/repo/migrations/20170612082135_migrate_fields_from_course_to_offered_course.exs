63e2454d6ea69aa885c26f5bbb746464a859f694
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-08-30 10:31:09 -0300

    Group course migration files.

--- priv/repo/migrations/20170612082135_migrate_fields_from_course_to_offered_course.exs
+++ priv/repo/migrations/20170612082135_migrate_fields_from_course_to_offered_course.exs
@@ -2,12 +2,6 @@ defmodule CoursePlanner.Repo.Migrations.MigrateFieldsFromCourseToOfferedCourse d
   use Ecto.Migration
 
   def change do
-    alter table(:courses) do
-      remove :session_duration
-      remove :syllabus
-      remove :number_of_sessions
-    end
-
     alter table(:offered_courses) do
       add :syllabus, :text
       add :number_of_sessions, :integer
--- priv/repo/migrations/20170830132937_create_course.exs
+++ priv/repo/migrations/20170830132937_create_course.exs
@@ -5,14 +5,8 @@ defmodule CoursePlanner.Repo.Migrations.CreateCourse do
     create table(:courses) do
       add :name, :string
       add :description, :string
-      add :number_of_sessions, :integer
-      add :session_duration, :time
-      add :syllabus, :string
-      add :status, :entity_status
-      add :deleted_at, :naive_datetime
 
       timestamps()
     end
-
   end
 end
