ab20e25b251d2a3c7d9f042e1c61430161a1dbf1
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-08-30 10:39:04 -0300

    Group offered course migration files.

--- priv/repo/migrations/20170830133638_create_offered_courses.exs
+++ priv/repo/migrations/20170830133638_create_offered_courses.exs
@@ -1,8 +1,10 @@
-defmodule CoursePlanner.Repo.Migrations.CreateOfferedCourse do
+defmodule CoursePlanner.Repo.Migrations.CreateOfferedCourses do
   use Ecto.Migration
 
   def change do
     create table(:offered_courses) do
+      add :syllabus, :text
+      add :number_of_sessions, :integer
       add :term_id, references(:terms, on_delete: :delete_all), null: false
       add :course_id, references(:courses, on_delete: :delete_all), null: false
 
@@ -10,5 +12,6 @@ defmodule CoursePlanner.Repo.Migrations.CreateOfferedCourse do
     end
     create index(:offered_courses, [:term_id])
     create index(:offered_courses, [:course_id])
+    create index(:offered_courses, [:term_id, :course_id], unique: true)
   end
 end
