dafdec1722effaf6e58c79123a140a1ee0cafe50
Jesse <jesse@gigalixir.com>
2017-08-31 06:23:33 -0700

    use REPLACE_OS_VARS style Distillery config vars.

--- config/prod.exs
+++ config/prod.exs
@@ -5,24 +5,24 @@ config :course_planner, CoursePlannerWeb.Endpoint,
   url: [scheme: "https", host: System.get_env("ENDPOINT_URL_HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: "${SECRET_KEY_BASE}"
 
 config :logger, level: :info
 
 config :course_planner, CoursePlanner.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  url: {:system, "DATABASE_URL"},
+  pool_size: String.to_integer("${POOL_SIZE}" || "10"),
   ssl: true
 
 config :coherence, CoursePlannerWeb.Coherence.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
-  api_key: System.get_env("SENDGRID_API_KEY")
+  api_key: {:system, "SENDGRID_API_KEY"}
 
 config :coherence,
-  email_from_name: System.get_env("EMAIL_FROM_NAME"),
-  email_from_email: System.get_env("EMAIL_FROM_EMAIL")
+  email_from_name: "${EMAIL_FROM_NAME}"
+  email_from_email: "${EMAIL_FROM_EMAIL}"
 
 config :course_planner, CoursePlanner.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
-  api_key: System.get_env("SENDGRID_API_KEY")
+  api_key: {:system, "SENDGRID_API_KEY"}
