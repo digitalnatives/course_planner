782ed5c9d3abaf270f0b6ceec47a5bdb76f1e8c1
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-09-06 17:12:35 +0200

    Adds negative permission tests for the bulk controller

---
+++ test/controllers/bulk_controller_test.exs
@@ -0,0 +1,55 @@
+defmodule CoursePlanner.BulkControllerTest do
+  use CoursePlannerWeb.ConnCase
+
+  import CoursePlanner.Factory
+  @valid_single_record %{"input" => %{"csv_data" => "Aname,AFamile,Anickname,A@a.com,student", "target" => "user", "title" => "user bulk creation"}}
+
+  setup(%{user_role: role}) do
+    user = insert(role)
+
+    conn =
+      Phoenix.ConnTest.build_conn()
+      |> assign(:current_user, user)
+    {:ok, conn: conn}
+  end
+
+  @moduletag user_role: :student
+  describe "settings functionality for student user" do
+    test "does not render new page", %{conn: conn} do
+      conn = get conn, bulk_path(conn, :new, target: "user", title: "Bulk Users")
+      html_response(conn, 403)
+    end
+
+    test "does not create bulk request for student user", %{conn: conn} do
+      conn = post conn, bulk_path(conn, :create, @valid_single_record)
+      html_response(conn, 403)
+    end
+  end
+
+  @moduletag user_role: :teacher
+  describe "settings functionality for teacher user" do
+    test "does not render new page", %{conn: conn} do
+      conn = get conn, bulk_path(conn, :new, target: "user", title: "Bulk Users")
+      html_response(conn, 403)
+    end
+
+    test "does not create bulk request for teacher user", %{conn: conn} do
+      conn = post conn, bulk_path(conn, :create, @valid_single_record)
+      html_response(conn, 403)
+    end
+  end
+
+  @moduletag user_role: :volunteer
+  describe "settings functionality for volunteer user" do
+    test "does not render new page", %{conn: conn} do
+      conn = get conn, bulk_path(conn, :new, target: "user", title: "Bulk Users")
+      html_response(conn, 403)
+    end
+
+    test "does not create bulk request for volunteer user", %{conn: conn} do
+      conn = post conn, bulk_path(conn, :create, @valid_single_record)
+      html_response(conn, 403)
+    end
+  end
+
+end
