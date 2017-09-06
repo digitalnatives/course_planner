f6302abd4820336784243986d8fd2632e1625c39
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-09-06 16:05:27 +0200

    fixes the dogma error

--- lib/course_planner_web/views/bulk_view.ex
+++ lib/course_planner_web/views/bulk_view.ex
@@ -3,10 +3,12 @@ defmodule CoursePlannerWeb.BulkView do
   use CoursePlannerWeb, :view
 
   def get_csv_field(target) do
+    required_fields =
       case target do
         "user" -> ["name*", "family name*", "nickname", "email*", "role*"]
         _      -> []
       end
-      |> Enum.join(",")
+
+    Enum.join(required_fields, ",")
   end
 end
