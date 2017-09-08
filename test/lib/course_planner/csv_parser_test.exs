6a9f79f143a67d39864e4a8b5934543ecab2488b
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-09-08 16:36:13 +0200

    adds more test for better coverage

--- test/lib/course_planner/csv_parser_test.exs
+++ test/lib/course_planner/csv_parser_test.exs
@@ -15,11 +15,45 @@ defmodule CoursePlanner.CsvParserTest do
     assert CsvParser.parse(",,", 3) == {:ok, [["","",""]]}
   end
 
-  test "when data and required columns do not match" do
+  test "when data and required columns do not match in a single line input" do
     assert CsvParser.parse("", 4) == {:error, "Input can not be empty."}
     assert CsvParser.parse("a,b,c", 4) == {:error, "Input data in row #1 is not matching the column number."}
     assert CsvParser.parse("a,b,c,d,e", 4) == {:error, "Input data in row #1 is not matching the column number."}
     assert CsvParser.parse("a,b,c,d,e,f,g,h,i", 4) == {:error, "Input data in row #1 is not matching the column number."}
+  end
+
+  test "when data and required columns do not match in a multi line input shows the first error" do
+    input_data = """
+    a,b,c,d
+    a,b,c
+    c,b
+    a
+    """
+
+    assert CsvParser.parse(input_data, 4) == {:error, "Input data in row #2 is not matching the column number."}
+  end
+
+  test "when data has excessive amount of data in one column shows the correct error line" do
+    input_data_type_1 = """
+    a,b,c,d
+    a,b,c,d
+    c,b,a,a
+    c,b,a,a,a
+    c,b,a,a,a
+    c,b,a,a,a
+    """
+
+    input_data_type_2 = """
+    a,b,c,d
+    a,b,c,d
+    c,b,a,a
+    c,b,a,a,
+    c,b,a,a,a
+    c,b,a,a,a
+    """
+
+    assert CsvParser.parse(input_data_type_1, 4) == {:error, "Input data in row #4 is not matching the column number."}
+    assert CsvParser.parse(input_data_type_2, 4) == {:error, "Input data in row #4 is not matching the column number."}
   end
 
   test "when input has one set of data" do
