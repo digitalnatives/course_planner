13ce0267a6e0c09df0a9c7cefd6f0246d9676a0d
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2017-09-20 10:39:26 -0300

    Add fallback controller.

---
+++ lib/course_planner_web/controllers/fallback_controller.ex
@@ -0,0 +1,15 @@
+defmodule CoursePlannerWeb.FallbackController do
+  use Phoenix.Controller
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(CoursePlannerWeb.ErrorView, "404.html")
+  end
+
+  def call(conn, _) do
+    conn
+    |> put_status(:internal_server_error)
+    |> render("500.html")
+  end
+end
