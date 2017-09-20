2a8adc48d3f93d6839158cf02b116eb170bcbe84
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-09-20 10:46:02 +0200

    Organizes class index page

--- lib/course_planner_web/controllers/class_controller.ex
+++ lib/course_planner_web/controllers/class_controller.ex
@@ -2,17 +2,21 @@ defmodule CoursePlannerWeb.ClassController do
   @moduledoc false
   use CoursePlannerWeb, :controller
 
-  alias CoursePlanner.{Classes.Class, Classes, Attendances}
+  alias CoursePlanner.{Classes.Class, Classes, Attendances, Terms.Term}
 
   import Canary.Plugs
   plug :authorize_controller
 
   def index(conn, _params) do
-    classes =
-      Class
-      |> Repo.all()
-      |> Repo.preload([:offered_course, offered_course: :term, offered_course: :course])
-    render(conn, "index.html", classes: classes)
+    query = from t in Term,
+    join: oc in assoc(t, :offered_courses),
+    join: c in assoc(oc, :classes),
+    preload: [offered_courses: :course, offered_courses: {oc, classes: c}],
+    order_by: [asc: t.start_date, asc: c.date, asc: c.starting_at, asc: c.finishes_at]
+
+    terms = Repo.all(query)
+
+    render(conn, "index.html", terms: terms)
   end
 
   def new(conn, _params) do
