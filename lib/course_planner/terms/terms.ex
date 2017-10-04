ace597e9537083cf24dbfe2b024aeb071e7ce2ef
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-10-04 14:38:38 +0200

    Fixes the query to filter the correct attendance

--- lib/course_planner/terms/terms.ex
+++ lib/course_planner/terms/terms.ex
@@ -133,11 +133,11 @@ defmodule CoursePlanner.Terms do
     Repo.all(from t in Term,
       join: oc in assoc(t, :offered_courses),
       join: co in assoc(oc, :course),
-      join: s in assoc(oc, :students),
       join: c in assoc(oc, :classes),
       join: a in assoc(c,  :attendances),
-      preload: [offered_courses: {oc, course: co, classes: {c, attendances: a}, students: s}],
-      where: s.id == ^student_id,
+      join: as in assoc(a, :student),
+      preload: [offered_courses: {oc, course: co, classes: {c, attendances: {a, student: as}}}],
+      where: as.id == ^student_id,
       order_by: [asc: t.start_date, asc: t.end_date, asc: co.name, asc: c.date, asc: c.starting_at])
   end
 end
