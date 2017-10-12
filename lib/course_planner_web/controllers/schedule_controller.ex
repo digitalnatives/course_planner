d0b1ca8f38a873fb4f4dab41a1e4365e6ec73b5b
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-10-12 16:58:44 +0200

    fix credo and dogma errors

--- lib/course_planner_web/controllers/schedule_controller.ex
+++ lib/course_planner_web/controllers/schedule_controller.ex
@@ -2,14 +2,16 @@ defmodule CoursePlannerWeb.ScheduleController do
   @moduledoc false
   use CoursePlannerWeb, :controller
 
+  alias Guardian.Plug
+
   import Canary.Plugs
   plug :authorize_controller
 
-  def show( %{assigns: %{current_user: current_user}} = conn, params) do
+  def show(%{assigns: %{current_user: current_user}} = conn, params) do
     jwt =
       conn
-      |> Guardian.Plug.api_sign_in(current_user)
-      |> Guardian.Plug.current_token()
+      |> Plug.api_sign_in(current_user)
+      |> Plug.current_token()
 
     conn
     |> render("show.html", params: params, jwt: jwt)
