70eb4c24d01e4e3c0f1126ff80afa3622faaa0ca
behrooz.torki@digitalnatives.hu <behrooz.torki@digitalnatives.hu>
2017-10-17 12:34:32 +0200

    fix dogma's error

--- lib/course_planner_web/views/auth/layout_view.ex
+++ lib/course_planner_web/views/auth/layout_view.ex
@@ -2,7 +2,7 @@ defmodule CoursePlannerWeb.Auth.LayoutView do
   @moduledoc false
   use CoursePlannerWeb, :view
 
-  def layout_title() do
+  def layout_title do
     Application.get_env(:course_planner, :auth_email_title)
   end
 end
